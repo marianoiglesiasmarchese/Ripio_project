017e26898f093fd05d69d50ca5b5fbddc75932e6
Mariano Iglesias Marchese <marianoiglesiasmarchese@gmail.com>
2018-02-26 20:51:05 -0300

    deletin .pyc files and cretion of Account class

---
+++ ripio_project/src/model/Account.py
@@ -0,0 +1,19 @@
+'''
+Created on 21 feb. 2018
+
+@author: miglesias
+'''
+from src.model.Currency import Currency
+
+class Account(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        currendy = Currency()
+        
